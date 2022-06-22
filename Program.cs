7273227ed9508c153a3295923f1216c663f9d0a9
Caleb Carey <caleb.n.carey@gmail.com>
2022-06-22 12:53:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace CodeJam2022
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => {
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build()
+            .Run();
+        }
+
+    }
+}
