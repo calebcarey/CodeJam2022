6eed2f3f308f1344f6090311afd42ead4f68c531
Caleb Carey <caleb.n.carey@gmail.com>
2022-06-22 13:37:28 -0600

    Client

---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CodeJam2022
+{
+    internal class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddWebSockets();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseWebSockets();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapConnectionHandler<WebSocketHandler>("/ws");
+            });
+        }
+    }
+}
